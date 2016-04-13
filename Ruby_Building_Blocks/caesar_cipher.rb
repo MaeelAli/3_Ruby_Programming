61b242955371ce28397aa4585a826334e5480aa1
MaeelAli <maeelali92@gmail.com>
2016-04-13 04:30:26 +0000

    added comments to Caesar Cipher Solution

--- Ruby_Building_Blocks/caesar_cipher.rb
+++ Ruby_Building_Blocks/caesar_cipher.rb
@@ -1,23 +1,37 @@
 def caesar_cipher (in_str, shift_factor)
 	
+	#define range of characters to be encrypted
 	lower = ('a'..'z').to_a
 	upper = lower.join("").upcase.split("")
 
+	#convert input string into array
 	letters = in_str.split("")
+
+	#initilize output variable
 	payload = []
+	
 	letters.each do |letter|
 		encrypted_text = letter
+
+		# if (a..z) 
 		if lower.include?(letter)
 			cipher_index = lower.index(letter) + shift_factor
 			encrypted_text = (shift_factor > 0) ? lower[cipher_index % 26] : lower[cipher_index]
+		
+		# else if (A..Z)
 		elsif upper.include?(letter)
 			cipher_index = upper.index(letter) + shift_factor
 			encrypted_text = (shift_factor > 0) ? upper[cipher_index % 26] : upper[cipher_index]
+		
+		# else don't need to encrypt
 		else
 			
 		end
+
+		#push encrypted character to payload
 		payload << encrypted_text
 	end
+	
 	payload = payload.join("")
 	puts payload
 end
