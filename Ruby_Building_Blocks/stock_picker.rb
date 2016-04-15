8a0431b98e90db430ee64b1708a2c598f32b5f35
MaeelAli <maeelali92@gmail.com>
2016-04-15 02:31:54 +0000

    added comments to stock_picker.rb

--- Ruby_Building_Blocks/stock_picker.rb
+++ Ruby_Building_Blocks/stock_picker.rb
@@ -1,10 +1,16 @@
 def stock_picker(stock_prices)
-               
+
+  #initialize best buy and sell hashs             
   best_buy = {"date" => 0, "price" => stock_prices[0]}
   best_sell = {"date" => 0, "price" => 0}
- 
+ 	
+ 	# for each day in stock prices array
   stock_prices.each_with_index do |buy_price, buy_day|
+
+  	# only if the current buy price <= your best buying price then
     if buy_price <= best_buy["price"]
+
+    	# for each day after current day, check that selling with this price would give better profit then best_profit, if so update best buy and sell date.
       stock_prices[(buy_day+1)..-1].each_with_index do |sell_price, sell_day|
         if (sell_price - buy_price) > (best_sell["price"] - best_buy["price"])
             best_sell["price"] = sell_price
@@ -12,15 +18,11 @@ def stock_picker(stock_prices)
             best_buy["price"] = buy_price
       			best_buy["date"] = buy_day
         end
-        puts "buy: #{buy_price} - #{buy_day} | sell: #{sell_price} - #{sell_day + buy_day} | curr_profit: #{sell_price - buy_price} | best_profit: #{best_sell["price"] - best_buy["price"]} "
-      end
-                                   
-    end
-                 
+        # puts "buy: #{buy_price} - #{buy_day} | sell: #{sell_price} - #{sell_day + buy_day} | curr_profit: #{sell_price - buy_price} | best_profit: #{best_sell["price"] - best_buy["price"]} "
+      end                                
+    end        
   end
-  #puts best_buy
-  #puts best_sell
-  #stock_prices[1..-1]
+
   timing = [best_buy["date"], best_sell["date"]]
   puts "For #{stock_prices}, the best time to buy and sell is: [#{timing[0]}, #{timing[1]}] to earn $#{best_sell["price"] - best_buy["price"]}"
 end
